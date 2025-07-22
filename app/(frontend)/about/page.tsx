00e54735eeeb444978a9a6aa6012f08c81435698
haniejnsh <h.janeshinpour1995@gmail.com>
2025-07-22 13:40:18 +0330

    complete crud2

--- app/(frontend)/about/page.tsx
+++ app/(frontend)/about/page.tsx
@@ -1,5 +1,30 @@
 export default function AboutPage() {
   return (
-    <div>About Page</div>
+    <div>
+      <div>
+        <h2>Introduction</h2>
+        <p>
+          Hello, I’m Hanieh Janeshinpour.
+
+          Since 2018, when I started my Master’s degree in Electronics, I developed a strong interest in programming. During my studies, I learned languages like Python, JavaScript, and Java.
+
+          After graduation, I worked in the electronics field for about two years. After getting married and encouraged by my husband, I pursued my passion for programming by joining a React.js bootcamp at Maktab Sharif in spring 2024. The course lasted around seven months, during which I completed several projects.
+
+          I began my professional career as a front-end developer in late autumn 2024. Due to circumstances in the country, I couldn’t continue in that position by late spring 2025. Afterward, I independently learned Next.js and worked on my portfolio project.
+
+          I’m passionate about continuous learning and aim to grow and contribute effectively to real-world projects.</p>
+        {/* <p>
+          سلام، من هانیه جانشین‌پور هستم.
+
+            از سال ۱۳۹۷ وقتی وارد مقطع کارشناسی ارشد الکترونیک شدم، علاقه‌ام به برنامه‌نویسی بیشتر شد. در طول تحصیلم، زبان‌هایی مثل پایتون، جاوااسکریپت و جاوا را یاد گرفتم.
+
+            پس از فارغ‌التحصیلی، حدود دو سال در حوزه الکترونیک کار کردم. بعد از ازدواج و با تشویق همسرم، به دنبال هدفم در برنامه‌نویسی رفتم و در بهار ۱۴۰۳ در بوت‌کمپ ریکت جی‌اس مکتب شریف شرکت کردم. این دوره حدود هفت ماه طول کشید و پروژه‌های متعددی انجام دادم.
+
+            کار حرفه‌ای‌ام را به عنوان توسعه‌دهنده فرانت‌اند از اواخر پاییز ۱۴۰۳ شروع کردم، اما به دلیل شرایط کشور، اواخر بهار ۱۴۰۴ فرصت ادامه کار نداشتم. سپس به صورت مستقل نکست جی‌اس را یاد گرفتم و روی پروژه پورتفولیوی خودم کار کردم.
+
+            من به یادگیری مداوم علاقه‌مندم و هدفم رشد و مشارکت در پروژه‌های واقعی است.
+        </p> */}
+      </div>
+    </div>
   )
 }
